649fd64819705e77425075077cab3b1e6dec508a
Rajni2002 <rajnikantdash12@gmail.com>
2024-05-15 01:24:57 +0530

    button layer fixes

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/navbar";
 export default function Home() {
     return (
         <>
-            <div className="home-bg px-5 md:px-10 py-6">
+            <div className="px-5 md:px-10 py-6">
                 <div className="star-field" >
                     <div className="layer" />
                     <div className="layer" />
